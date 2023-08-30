64ba9a20d4a34eca01669a92e5d757531a86611d
Dell <Dell@DESKTOP-NOUQ59H>
2023-08-30 11:48:09 -0600

    links to social media added

--- src/app/portfolio/home/home.component.ts
+++ src/app/portfolio/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { BioService } from 'src/app/core/services/bio.service';
 
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
